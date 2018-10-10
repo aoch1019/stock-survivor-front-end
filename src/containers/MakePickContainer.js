066caa6cc8579ffa6d5425549cfd8621a4ebc527
Andrew Och <andrewoch95@gmail.com>
2018-10-10 16:47:09 -0400

    Added submit option for pick

--- src/containers/MakePickContainer.js
+++ src/containers/MakePickContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import StockTable from '../components/StockTable'
-import Search from '../components/Search'
+import StockTable from '../components/MakePickContainer-components/StockTable'
+import Search from '../components/MakePickContainer-components/Search'
+import SubmitField from '../components/MakePickContainer-components/SubmitField'
 
 export default class MakePickContainer extends Component{
   constructor(props){
@@ -13,12 +14,14 @@ export default class MakePickContainer extends Component{
 
     this.handleSearchInputChange = this.handleSearchInputChange.bind(this)
     this.selectStock = this.selectStock.bind(this)
+    this.handleNoClick = this.handleNoClick.bind(this)
+    this.handleYesClick = this.handleYesClick.bind(this)
   }
 
   selectStock(stockInfo){
     this.setState({
       currSelection: stockInfo
-    }, () => console.log(this.state.currSelection))
+    })
   }
 
   handleSearchInputChange(event){
@@ -27,11 +30,26 @@ export default class MakePickContainer extends Component{
     })
   }
 
+  handleNoClick(){
+    this.setState({
+      currSelection: null
+    })
+  }
+
+  handleYesClick(){
+    alert("Pick Submitted!")
+  }
+
   render(){
     return(
       <React.Fragment>
-        < Search handleSearchInputChange={this.handleSearchInputChange}/>
-        < StockTable stocks={this.props.stocks} currSearch={this.state.currSearch} selectStock={this.selectStock}/>
+        < Search handleSearchInputChange={this.handleSearchInputChange} />
+      {
+        this.state.currSelection !== null
+        &&
+        < SubmitField currSelection={this.state.currSelection} handleYesClick={this.handleYesClick} handleNoClick={this.handleNoClick}/>
+      }
+        < StockTable stocks={this.props.stocks} currSearch={this.state.currSearch} selectStock={this.selectStock} />
       </React.Fragment>
     )
   }
